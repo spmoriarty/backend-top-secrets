ba746e7d136fdf0250fff6ecb13b2943bcc7e799
Sean Moriarty <spmoriarty1@gmail.com>
2022-08-09 16:10:35 -0700

    test built out, working on creating users

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,10 @@
 -- Use this file to define your SQL tables
+DROP TABLE IF EXISTS users;
 -- The SQL in this file will be executed when you run `npm run setup-db`
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password_hash TEXT NOT NULL
+);
