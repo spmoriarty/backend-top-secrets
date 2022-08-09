9a8055559473e357d826e0783602c3ac5cce3e46
Sean Moriarty <spmoriarty1@gmail.com>
2022-08-09 15:21:32 -0700

    created first test for adding user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,36 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const { request } = require('../lib/app');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
-describe('backend-express-template routes', () => {
+describe('backend top secrets routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  
+});
+const newUser = {
+  firstName: 'Testing',
+  lastName: 'User',
+  email: 'lamp@shade.com',
+  password: 12345,
+}
+
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(newUser);
+    const { firstName, lastName, email } = newUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
+
+  //NEW TEST HERE
+
   afterAll(() => {
     pool.end();
   });
