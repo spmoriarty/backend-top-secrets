43fc49a702f25a78541813d9446db22c943f7d46
Sean Moriarty <spmoriarty1@gmail.com>
2022-08-09 16:39:09 -0700

    password was not returning as strings

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,24 +1,25 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-const { request } = require('../lib/app');
+const request = require('supertest');
 const app = require('../lib/app');
-const UserService = require('../lib/services/UserService');
+// const UserService = require('../lib/services/UserService');
+const newUser = {
+  firstName: 'Testing',
+  lastName: 'User',
+  email: 'lamp@shade.com',
+  password: '12345',
+};
 
 describe('backend top secrets routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
   
-});
-const newUser = {
-  firstName: 'Testing',
-  lastName: 'User',
-  email: 'lamp@shade.com',
-  password: 12345,
-}
+
 
   it('creates a new user', async () => {
-    const res = await request(app).post('/api/v1/users').send(newUser);
+    const agent = request.agent(app);
+    const res = await agent.post('/api/v1/users').send(newUser);
     const { firstName, lastName, email } = newUser;
 
     expect(res.body).toEqual({
