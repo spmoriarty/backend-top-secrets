dc2c69c9fccdbd27e84b96f1ea8883dba2fa76c0
Sean Moriarty <spmoriarty1@gmail.com>
2022-08-10 14:19:55 -0700

    test for ending session / deleting session

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,25 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
-// const UserService = require('../lib/services/UserService');
+
+
+
 const newUser = {
   firstName: 'Testing',
   lastName: 'User',
   email: 'lamp@shade.com',
   password: '12345',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? newUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...newUser, ...userProps });
+  const { email } = user;
+  await agent.post('/api/v1/users/session').send({ email, password });
+  return [agent, user];
 };
 
 describe('backend top secrets routes', () => {
@@ -40,6 +52,18 @@ describe('backend top secrets routes', () => {
     expect(res.status).toBe(200);
   });
 
+  it('#Delete /session cookie', async () => {
+    const [agent] = await registerAndLogin();
+    const res = await agent.get('/api/v1/secrets');
+    expect(res.status).toBe(200) ;
+    const delUser = await agent.delete('/api/v1/users/session');
+    expect(delUser.body).toEqual({
+      success: true,
+      message: 'Signed out successfully!' });
+
+    const getUser =  await request(app).get('api/v1/secrets');
+    expect(getUser.status).toBe(401);
+  });
 
   //NEW TEST HERE
 
